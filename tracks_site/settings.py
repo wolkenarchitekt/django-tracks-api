e52f70861225eb4e1357f28fa6fe595548b95a3d
Ingo Fischer <mail@ingofischer.de>
2020-01-20 22:37:02 +0100

    Add Django sessions module

--- tracks_site/settings.py
+++ tracks_site/settings.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
+    'django.contrib.sessions',
     'django.contrib.staticfiles',
     'tracks_api',
 ]
