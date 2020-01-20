d53465662dd164b8a564e6562a898ff3ba79ddc3
Ingo Fischer <mail@ingofischer.de>
2020-01-20 22:38:21 +0100

    Disallow unauthenticated access to audit log saving

--- tracks_api/urls.py
+++ tracks_api/urls.py
@@ -5,13 +5,6 @@ from django.contrib import admin
 from django.urls import path
 
 
-# Allow passwordless login to Admin
-class AccessUser(object):
-    has_module_perms = has_perm = __getattr__ = lambda s, *a, **kw: True
-
-
-admin.site.has_permission = lambda r: setattr(r, 'user', AccessUser()) or True
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
