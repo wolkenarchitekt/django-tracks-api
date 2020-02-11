f15eaf0c6ae16e6e21e18b99e6c405c78102dc60
Ingo Fischer <mail@ingofischer.de>
2020-02-11 21:56:07 +0100

    Fix flake8 print linting

--- tracks_api/management/commands/import.py
+++ tracks_api/management/commands/import.py
@@ -1,9 +1,12 @@
+import logging
 import os
 import sys
 
 from django.core.management.base import BaseCommand
 
 from tracks_api.tracks_import import import_tracks_to_db
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -15,9 +18,10 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        logging.basicConfig(level=logging.INFO)
         for music_dir in options["music-dirs"]:
             if not os.path.isdir(music_dir):
-                print(f"Path not found: {music_dir}")
+                logger.warning(f"Path not found: {music_dir}")
                 sys.exit(1)
 
             import_tracks_to_db(music_dir)
