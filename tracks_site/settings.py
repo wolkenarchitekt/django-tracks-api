751a3e2b0bb87b314f6b3c6ccf8a2ee7db51ccd9
Ingo Weinmann <mail@ingoweinmann.de>
2021-01-20 00:12:14 +0100

    Add cors headers to allow access by opentunes-webapp

--- tracks_site/settings.py
+++ tracks_site/settings.py
@@ -35,6 +35,7 @@ DATABASES = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -57,6 +58,9 @@ TEMPLATES = [
         },
     }
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 STATIC_URL = os.environ.get("STATIC_URL", "/static/")
 STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
